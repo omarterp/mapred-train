794e16f96d9540a5ece3e66dd1b80ec7548af623
Eric Perales <omarterp@gmail.com>
2017-02-11 07:59:22 -0500

    readied for job testing - to AWS!

--- src/main/java/MapRedTrain.java
+++ src/main/java/MapRedTrain.java
@@ -1,20 +1,19 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by omart on 2/8/2017.
  */
 public class MapRedTrain {
 
-    private final String delimiters = "[ \t,;\\.\\?\\!-:@\\[\\](){}_\\*/]";
-    private final List<String> stopWords = new ArrayList<>();
+    private String delimiters; //"[ \t,;\\.\\?\\!-:@\\[\\](){}_\\*/]";
+    private List<String> stopWords = new ArrayList<>();
+    private Map<String, List<Integer>> words = new HashMap<>();
 
     private enum FilePath {
-        DELIMITER_FILE("tmp/delimiter.txt"),
+        DELIMITER_FILE("tmp/delimiters.txt"),
         STOP_WORD_FILE("tmp/stopwords.txt"),
-        TITLE_FILE("tmp/title-a");
+        TITLE_FILE("tmp/titles-a");
 
         private String path;
 
@@ -26,6 +25,13 @@ public class MapRedTrain {
             return path;
         }
 }
+
+    public boolean setup() {
+        stopWords = Arrays.asList(loadFile(FilePath.STOP_WORD_FILE.getPath()).split("\n"));
+        delimiters = loadFile(FilePath.DELIMITER_FILE.getPath());
+
+        return true;
+    }
 
     public String loadFile(String path) {
         String line;
@@ -46,15 +52,78 @@ public class MapRedTrain {
         return everything.toString();
     }
 
+    public Map<String, List<Integer>> map(String inputFile) {
+
+        String line;
+
+        try {
+            try(BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+                while((line = br.readLine()) != null) {
+                    // TODO Mapper Code Begins here
+                    // Split the line based on delimiters
+
+                    StringTokenizer tokens = new StringTokenizer(line, delimiters);
+                    while(tokens.hasMoreTokens()) {
+                        String token = tokens.nextToken().trim().toLowerCase();
+                        if(!stopWords.contains(token.trim().toLowerCase())) {
+                            if(!words.containsKey(token)) {
+                                List<Integer> values = new ArrayList<>();
+                                values.add(1);
+                                words.put(token, values);
+                            }
+                            else {
+                                words.get(token).add(1);
+                            }
+                            //System.out.println(token);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for(Map.Entry<String, List<Integer>> entry : words.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        return words;
+    }
+
+    /**
+     * Reduces the mapped words and frequency
+     *
+     * @return KV Pair of word and frequency
+     */
+    public Map<String, Integer> reduce() {
+        Map<String, Integer> wordCounts = new HashMap<>();
+
+        // Iterate over the words object and sum word occurrences
+        for(Map.Entry<String, List<Integer>> word : words.entrySet()) {
+            int freq = 0;
+
+            for(int hit : word.getValue()) {
+                freq += 1;
+            }
+            wordCounts.put(word.getKey(), freq);
+        }
+
+        return wordCounts;
+    }
+
     public static void main(String[] args) {
 
         MapRedTrain mrt = new MapRedTrain();
+        mrt.setup();
 
-        List<String> stopWords = Arrays.asList(mrt.loadFile(FilePath.STOP_WORD_FILE.getPath()).split("\n"));
-//        String delimiters = mrt.loadFile(FilePath.STOP_WORD_FILE.getPath());
-//
-        System.out.println(stopWords);
-//        System.out.println(delimiters);
+        mrt.map(FilePath.TITLE_FILE.getPath());
+        System.out.println(mrt.reduce());
+
+//        List<String> stopWords = Arrays.asList(mrt.loadFile(FilePath.STOP_WORD_FILE.getPath()).split("\n"));
+//        String delimiters = mrt.loadFile(FilePath.DELIMITER_FILE.getPath());
+
+        System.out.println(mrt.stopWords);
+        System.out.println(mrt.delimiters);
 
 
     }
