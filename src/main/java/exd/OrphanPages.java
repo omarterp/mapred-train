dfc8d52263420fde5c31ea4d43ddf1b73f26e33a
Eric Perales <omarterp@gmail.com>
2017-02-19 11:55:26 -0500

    exd complete

--- src/main/java/exd/OrphanPages.java
+++ src/main/java/exd/OrphanPages.java
@@ -1,10 +1,12 @@
 package exd;
 
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -19,9 +21,7 @@ import org.apache.hadoop.util.ToolRunner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 // >>> Don't Change
 public class OrphanPages extends Configured implements Tool {
@@ -35,10 +35,9 @@ public class OrphanPages extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
         Configuration conf = this.getConf();
-        FileSystem fs = FileSystem.get(conf);
 
         Job job = Job.getInstance(conf, "Orphan Pages");
-        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(IntWritable.class);
 
         job.setMapperClass(LinkCountMap.class);
@@ -47,59 +46,43 @@ public class OrphanPages extends Configured implements Tool {
         FileInputFormat.setInputPaths(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
+        job.setJarByClass(OrphanPages.class);
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
-    public static String readHDFSFile(String path, Configuration conf) throws IOException {
-        Path pt = new Path(path);
-
-        try(FileSystem fs = FileSystem.get(pt.toUri(), conf);
-            FSDataInputStream file = fs.open(pt);
-            BufferedReader buffR = new BufferedReader(new InputStreamReader(file))) {
-
-    }
-
     public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
-
-        @Override
-        protected void setup(Context context) throws IOException, InterruptedException {
-            super.setup(context);
-
-            "2: 4567 7583 9382 832",
-                    "4567: 32 847 921 2827",
-                    "23: 324 73 8387 382"
-        };
-
-        // Mapper Code
-            List<Integer> linkedPage = new ArrayList<>();
-            for(String line : lines) {
-                String referrer = line.split(":")[0];
-                String linkedPages = line.split(":")[1];
-                System.out.println(linkedPages);
-
-                for(String page : linkedPages.split("\\s")) {
-                    String cleanPage = page.trim();
-                    if(cleanPage.length() > 0)
-                        linkedPage.add(Integer.parseInt(cleanPage));
-                }
-
-                System.out.println(referrer + " : " + linkedPage);
-            }
-            // Mapper Code
-
-
-        }
-
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            //TODO
+            String KV_DELIMITER = ":";
+            String VALUE_DELIMITER = "\\s";
+
+            String referrer = value.toString().split(KV_DELIMITER)[0];
+            String linkedPages = value.toString().split(KV_DELIMITER)[1];
+
+            for(String page : linkedPages.split(VALUE_DELIMITER)) {
+                String cleanPage = page.trim();
+                if(cleanPage.length() > 0) {
+                    // linked page
+                    context.write(new IntWritable(Integer.parseInt(cleanPage)), new IntWritable(1));
+                }
+            }
+            // referrer, which may not have any links - setting to 0
+            context.write(new IntWritable(Integer.parseInt(referrer)), new IntWritable(0));
         }
     }
 
     public static class OrphanPageReduce extends Reducer<IntWritable, IntWritable, IntWritable, NullWritable> {
         @Override
         public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            //TODO
+            int linkedTotal = 0;
+
+            for(IntWritable hit : values) {
+                linkedTotal += hit.get();
+            }
+
+            // Orphaned page
+            if(linkedTotal == 0)
+                context.write(key, NullWritable.get());
         }
     }
 }
